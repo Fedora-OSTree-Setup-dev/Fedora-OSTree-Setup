c039c9629eea2d608e5ae1c919878012ccde4fa6
James Aaron Erang <iaacornus.devel@gmail.com>
2022-12-25 22:48:46 +0800

    include clamtk and flatseal

--- src/data/app_for_install.py
+++ src/data/app_for_install.py
@@ -5,26 +5,39 @@ from dataclasses import dataclass
 class GetApps:
     """Apps to be installed"""
 
+    #* takes the name of the app, then a dictionary which includes the
+    #* application id, and then a simple description for the given app
     recommend_list: dict[str, dict[str, str]] = {
-            "mailspring": {
+            "Mailspring": {
                     "aid": "com.getmailspring.Mailspring",
                     "simple_description": "A simple email client."
                 },
-            "libreoffice": {
+            "LibreOffice": {
                     "aid": "org.libreoffice.LibreOffice",
                     "simple_description": "Office suite."
                 },
-            "vlc": {
+            "VLC": {
                     "aid": "org.videolan.VLC",
                     "simple_description": "Video player."
                 },
-            "okular": {
+            "Okular": {
                     "aid": "org.kde.okular",
                     "simple_description": "A document viewer."
                 },
-            "gimp": {
+            "GIMP": {
                     "aid": "org.gimp.GIMP",
                     "simple_description": "Photo editing application."
+                },
+            "ClamTk": {
+                    "aid": "com.gitlab.davem.ClamTk",
+                    "simple_description": "Front end for ClamAV."
+                },
+            "FlatSeal": {
+                    "aid": "com.github.tchx84.Flatseal",
+                    "simple_description": (
+                            "GUI for managing flatpak"
+                            " applications permission"
+                        )
                 }
         }
     selected_app: list[str] = None
