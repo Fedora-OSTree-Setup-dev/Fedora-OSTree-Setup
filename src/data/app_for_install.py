95832fb3b85a51c3197bbcd88198f07e75796c4d
James Aaron Erang <iaacornus.devel@gmail.com>
2022-12-25 21:56:05 +0800

    include descriptions for app

--- src/data/app_for_install.py
+++ src/data/app_for_install.py
@@ -5,11 +5,26 @@ from dataclasses import dataclass
 class GetApps:
     """Apps to be installed"""
 
-    recommend_list: dict[str, str] = {
-            "mailspring": "com.getmailspring.Mailspring",
-            "libreoffice": "org.libreoffice.LibreOffice",
-            "vlc": "org.videolan.VLC",
-            "okular": "org.kde.okular",
-            "gimp": "org.gimp.GIMP"
+    recommend_list: dict[str, dict[str, str]] = {
+            "mailspring": {
+                    "aid": "com.getmailspring.Mailspring",
+                    "simple_description": "A simple email client."
+                },
+            "libreoffice": {
+                    "aid": "org.libreoffice.LibreOffice",
+                    "simple_description": "Office suite."
+                },
+            "vlc": {
+                    "aid": "org.videolan.VLC",
+                    "simple_description": "Video player."
+                },
+            "okular": {
+                    "aid": "org.kde.okular",
+                    "simple_description": "A document viewer."
+                },
+            "gimp": {
+                    "aid": "org.gimp.GIMP",
+                    "simple_description": "Photo editing application."
+                }
         }
     selected_app: list[str] = None
