88a9b90f06f3be16496160e1ecb2df9fdd5c5548
James Aaron Erang <iaacornus.devel@gmail.com>
2022-12-25 22:10:15 +0800

    module for checking the gpu in the system

---
+++ src/utils/core/gpu_install.py
@@ -0,0 +1,34 @@
+from subprocess import (
+    Popen,
+    CalledProcessError,
+    PIPE,
+    check_output
+)
+
+from src.utils.log.logger import Logger
+
+
+def fetch_gpu(log: Logger) -> str | None:
+    """Fetch the GPU of the system.
+
+    Args:
+        log -- instance of Logger
+
+    Returns:
+        The name of GPU or None.
+    """
+
+    try:
+        lspci_out = Popen(("lspci"), stdout=PIPE)
+        gpu_name: str = check_output(
+                ["grep", "-i", "VGA"], stdin=lspci_out.stdout
+            )
+        lspci_out.wait()
+
+        if lspci_out.returncode != 0:
+            raise SystemExit(["lspci"], lspci_out.returncode)
+    except CalledProcessError as Err:
+        log.logger("e", f"{Err}. Command lspci failed to execute.")
+        return None
+    else:
+        return gpu_name
