8660ccdeda8e1381cbd142b04f4e7da7047985ae
James Aaron Erang <iaacornus.devel@gmail.com>
2022-12-27 21:04:57 +0800

    for fetching of config file from github

---
+++ src/utils/misc/fetch_config.py
@@ -0,0 +1,46 @@
+from typing import NoReturn
+
+from requests import get
+
+from src.utils.log.logger import Logger
+
+
+def fetch_missing_config(
+        log: Logger, conf: str, CONF_PATH: str
+    ) -> None | NoReturn:
+    """Downloads the original config file from github if not found.
+
+    Args:
+        log -- instance of Logger
+        conf -- name of the missing config
+        CONF_PATH -- path of the config file
+    """
+
+    conf_link: str = ""
+
+    trial: int
+    for trial in range(3):
+        try:
+            log.logger(
+                "I", "Fetching the missing config file from Github."
+            )
+            with get(conf_link, stream=True) as d_file:
+                with open(
+                        f"{CONF_PATH}/{conf}", "wb"
+                    ) as conf_file:
+                    for chunk in d_file.iter_content(chunk_size=1024):
+                        if chunk:
+                            conf_file.write(chunk)
+        except (ConnectionError, IOError, PermissionError) as Err:
+            if trial < 2:
+                continue
+
+            log.logger(
+                "E", f"{Err}. Cannot download {conf}, aborting ..."
+            )
+        else:
+            log.logger("I", f"Sucessfully fetched: {conf}.")
+            return None
+
+    raise SystemExit
+
