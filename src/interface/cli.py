94609958f84b45e7f5d2b76bcb30f855a4b20959
James Aaron Erang <iaacornus.devel@gmail.com>
2022-12-31 00:56:31 +0800

    initial commit in cli of program

---
+++ src/interface/cli.py
@@ -0,0 +1,19 @@
+from argparse import ArgumentParser
+
+from src.utils.log.logger import Logger
+
+class Cli:
+    """Commandline interface of the program."""
+
+    def __init__(self) -> None:
+        self.log: Logger = Logger()
+
+        self.parser: ArgumentParser = ArgumentParser(
+                prog="ostree-setup",
+                usage="ostree-setup [ARGUMENTS]",
+                description=(
+                        "A small program making the install of Fedora "
+                        "Silverblue / Kionite easy. It lets you choose "
+                        "what to install or set. "
+                    )
+            )
