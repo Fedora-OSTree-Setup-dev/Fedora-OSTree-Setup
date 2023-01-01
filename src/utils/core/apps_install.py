76df02cdb07dc23e23db6e610b7644fc39631415
James Aaron Erang <iaacornus.devel@gmail.com>
2023-01-01 22:27:53 +0800

    for installation of recommended applications

--- src/utils/core/apps_install.py
+++ src/utils/core/apps_install.py
@@ -1,10 +1,18 @@
+from rich.panel import Panel
+from rich.align import Align
+from rich.text import Text
+from rich.console import Console
+
 from src.utils.shared.exec import execute_command
 from src.utils.shared.uinput import uinput
 from src.utils.shared.log.logger import Logger
 
 
 def app_install(
-        log: Logger, app_for_install: dict[str, dict[str, str]]
+        log: Logger,
+        console: Console,
+        app_for_install: dict[str, dict[str, str]],
+        verbose: bool = False
     ) -> None:
     """For installation of recommended applications selected by the user.
 
@@ -14,4 +22,51 @@ def app_install(
             their application id (aid) and description
     """
 
-    pass
+    appindex: dict[int, str] = {
+            index: aid for index, aid in zip(
+                range(len(app_for_install.items())), app_for_install.keys()
+            )
+        }
+
+    console.print(
+        Panel(
+            Align(
+                Text(
+                    "Install applications from flathub",
+                    justify="center"
+                ),
+                vertical="middle",
+                align="center"
+            ),
+            title="[bold]Recommended Flatpak Applications[/bold]"
+        )
+    )
+
+    appname: str
+    for index, appname in appindex.items():
+        console.print(
+            (
+                f"[bold cyan]{index:4}[/bold cyan] "
+                f"[bold]{appname}[/bold] -- "
+                f"{app_for_install.get(appname).get('sdesc')}"
+            )
+        )
+
+    selected_app: list[int] = uinput(
+            console, "Input the number of applications to install", 2
+        )
+
+    aindex: int
+    for aindex in selected_app:
+        sapp_id: str = app_for_install.get(
+                appindex.get(aindex)).get("aid"
+            )
+        install_cmd: list[str] = [
+                "flatpak",
+                "install",
+                "flatpak",
+                sapp_id
+            ]
+        execute_command(log, install_cmd, verbose)
+
+    return None
