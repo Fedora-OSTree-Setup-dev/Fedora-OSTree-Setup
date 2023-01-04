39cb45673e6c60eeb118c8f08f50d9b9a0296592
James Aaron Erang <iaacornus.devel@gmail.com>
2023-01-05 04:31:39 +0800

    create alias instead of new type

--- src/misc/alias.py
+++ src/misc/alias.py
@@ -1,5 +1,2 @@
-from typing import NewType
-
-
-AppData = NewType("AppData", dict[str, dict[str, str]])
-AppIndex = NewType("AppIndex", dict[int, str])
+AppData = dict[str, dict[str, str]]
+AppIndex = dict[int, str]
