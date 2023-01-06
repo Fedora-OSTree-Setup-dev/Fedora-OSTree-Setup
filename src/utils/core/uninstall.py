19f5fe9d60b0548b38309989bc42e4a7b1f0280d
James Aaron Erang <iaacornus.devel@gmail.com>
2023-01-06 14:51:39 +0800

    turn into class to organize the methods

--- src/utils/core/uninstall.py
+++ src/utils/core/uninstall.py
@@ -1,36 +1,41 @@
 from src.utils.shared.misc.section import section
 
 
-def uninstall_apps(
-        prog_arr: list[str], progtype: str
-    ) -> list[list[str]] | list[str]:
-    """Uninstall preinstalled flatpak applications.
 
-    Args:
-        prog_arr -- list of apps to uninstall
-        progtype -- the type of application to remove
+class Uninstall:
+    def __init__(self) -> None:
+        pass
 
-    Returns:
-        An array of the appropriate uninstall commands or array of the
-        applications to uninstall
-    """
+    def uninstall_apps(
+            prog_arr: list[str], progtype: str
+        ) -> list[list[str]] | list[str]:
+        """Uninstall preinstalled flatpak applications.
 
-    t_fcmd: list[list[str]] = []
+        Args:
+            prog_arr -- list of apps to uninstall
+            progtype -- the type of application to remove
 
-    prog: str
-    for prog in prog_arr:
-        section(
-            "uninstallation of unnecessary preinstalled programs",
-            f"listed unnecessary preinstalled programs ({progtype})"
-        )
-        uninstall_cmd: list[str] = [
-                "flatpak",
-                "uninstall",
-                prog,
-                "--delete-data",
-                "--system",
-                "--assumeyes"
-            ]
-        t_fcmd.append(uninstall_cmd)
+        Returns:
+            An array of the appropriate uninstall commands or array of the
+            applications to uninstall
+        """
 
-    return t_fcmd
+        t_fcmd: list[list[str]] = []
+
+        prog: str
+        for prog in prog_arr:
+            section(
+                "uninstallation of unnecessary preinstalled programs",
+                f"listed unnecessary preinstalled programs ({progtype})"
+            )
+            uninstall_cmd: list[str] = [
+                    "flatpak",
+                    "uninstall",
+                    prog,
+                    "--delete-data",
+                    "--system",
+                    "--assumeyes"
+                ]
+            t_fcmd.append(uninstall_cmd)
+
+        return t_fcmd
