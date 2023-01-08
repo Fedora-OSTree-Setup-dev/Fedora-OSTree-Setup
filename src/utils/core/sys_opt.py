85fe78bad1d1ae898b13fe4cf2dfc7b81b0979df
James Aaron Erang <iaacornus.devel@gmail.com>
2023-01-08 23:59:03 +0800

    include constructor
    the constructor takes a parameters of
    	log
    	console
    	verbose

--- src/utils/core/sys_opt.py
+++ src/utils/core/sys_opt.py
@@ -8,16 +8,23 @@ from src.utils.shared.log.logger import Logger
 class SysOpt:
     """For system optimizations"""
 
-    def disable_services(
-            console: Console, log: Logger, verbose: bool = False
+    def __init__(
+            self, log: Logger, console: Console, verbose: bool = False
         ) -> None:
-        """Disable autostart/systemd services that is not necessary
-
+        """
         Args:
-            console -- instance of console
             log -- instance of logger
+            console -- instance of console
             verbose -- whether to show the stdout of cmds
         """
+
+        self.log: Logger = log
+        self.console: Console = console
+
+        self.verbose: bool = verbose
+
+    def disable_services(self) -> None:
+        """Disable autostart/systemd services that is not necessary"""
 
         cmd_arr: list[list[str]] = [
                 [
@@ -35,5 +42,5 @@ class SysOpt:
 
         cmd: str
         for cmd in cmd_arr:
-            if uinput(console, f"Execute: {cmd}", 1):
-                exec_cmd(log, cmd, verbose)
+            if uinput(self.console, f"Execute: {cmd}", 1):
+                exec_cmd(self.log, cmd, self.verbose)
