71fcca058fa584f316d644d5feba93b0aebed011
James Aaron Erang <iaacornus.devel@gmail.com>
2023-01-08 23:56:22 +0800

    turn into a class only for system optimization

--- src/utils/core/sys_opt.py
+++ src/utils/core/sys_opt.py
@@ -5,32 +5,35 @@ from src.utils.shared.misc.uinput import uinput
 from src.utils.shared.log.logger import Logger
 
 
-def disable_services(
-        console: Console, log: Logger, verbose: bool = False
-    ) -> None:
-    """Disable autostart/systemd services that is not necessary
+class SysOpt:
+    """For system optimizations"""
 
-    Args:
-        console -- instance of console
-        log -- instance of logger
-        verbose -- whether to show the stdout of cmds
-    """
+    def disable_services(
+            console: Console, log: Logger, verbose: bool = False
+        ) -> None:
+        """Disable autostart/systemd services that is not necessary
 
-    cmd_arr: list[list[str]] = [
-            [
-                "sudo",
-                "systemctl",
-                "disable",
-                "NetworkManager-wait-online.service"
-            ],
-            [
-                "sudo",
-                "rm",
-                "/etc/xdg/autostart/org.gnome.Software.desktop"
+        Args:
+            console -- instance of console
+            log -- instance of logger
+            verbose -- whether to show the stdout of cmds
+        """
+
+        cmd_arr: list[list[str]] = [
+                [
+                    "sudo",
+                    "systemctl",
+                    "disable",
+                    "NetworkManager-wait-online.service"
+                ],
+                [
+                    "sudo",
+                    "rm",
+                    "/etc/xdg/autostart/org.gnome.Software.desktop"
+                ]
             ]
-        ]
 
-    cmd: str
-    for cmd in cmd_arr:
-        if uinput(console, f"Execute: {cmd}", 1):
-            exec_cmd(log, cmd, verbose)
+        cmd: str
+        for cmd in cmd_arr:
+            if uinput(console, f"Execute: {cmd}", 1):
+                exec_cmd(log, cmd, verbose)
