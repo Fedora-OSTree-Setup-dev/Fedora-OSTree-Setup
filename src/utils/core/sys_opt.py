db67bc10c1a735baa2b8f73bb66281eebc0a79c1
James Aaron Erang <iaacornus.devel@gmail.com>
2023-01-08 23:54:57 +0800

    for disabling of system services/autostart programs

---
+++ src/utils/core/sys_opt.py
@@ -0,0 +1,36 @@
+from rich.console import Console
+
+from src.utils.shared.exec import exec_cmd
+from src.utils.shared.misc.uinput import uinput
+from src.utils.shared.log.logger import Logger
+
+
+def disable_services(
+        console: Console, log: Logger, verbose: bool = False
+    ) -> None:
+    """Disable autostart/systemd services that is not necessary
+
+    Args:
+        console -- instance of console
+        log -- instance of logger
+        verbose -- whether to show the stdout of cmds
+    """
+
+    cmd_arr: list[list[str]] = [
+            [
+                "sudo",
+                "systemctl",
+                "disable",
+                "NetworkManager-wait-online.service"
+            ],
+            [
+                "sudo",
+                "rm",
+                "/etc/xdg/autostart/org.gnome.Software.desktop"
+            ]
+        ]
+
+    cmd: str
+    for cmd in cmd_arr:
+        if uinput(console, f"Execute: {cmd}", 1):
+            exec_cmd(log, cmd, verbose)
