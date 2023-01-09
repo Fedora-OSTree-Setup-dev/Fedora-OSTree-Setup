cc2f6c44a7eec011cbdc83061268691996a4c028
James Aaron Erang <iaacornus.devel@gmail.com>
2023-01-09 14:03:04 +0800

    create a method for de based optimizations
    this method gives a specific command/tasks that is exclusive for
    particular de

--- src/utils/core/sys_opt.py
+++ src/utils/core/sys_opt.py
@@ -1,6 +1,7 @@
 from rich.console import Console
 
 from src.utils.shared.exec import exec_cmd
+from src.utils.shared.fetch_env import fetch_env
 from src.utils.shared.misc.uinput import uinput
 from src.utils.shared.log.logger import Logger
 
@@ -33,14 +34,31 @@ class SysOpt:
                     "disable",
                     "NetworkManager-wait-online.service"
                 ],
-                [
-                    "sudo",
-                    "rm",
-                    "/etc/xdg/autostart/org.gnome.Software.desktop"
-                ]
             ]
 
         cmd: str
+        for cmd in cmd_arr:
+            if uinput(self.console, f"Execute: {cmd}", 1):
+                exec_cmd(self.log, cmd, self.verbose)
+
+    def de_based_opt(self) -> None:
+        """For disabling of autostart exclusive on specific de"""
+        match fetch_env(
+                self.log, self.console, "XDG_SESSION_DESKTOP"
+            ).lower():
+            case "gnome":
+                cmd_arr: list[list[str]] = [
+                        [
+                            "sudo",
+                            "rm",
+                            "/etc/xdg/autostart/org.gnome.Software.desktop"
+                        ]
+                    ]
+            case "kde":
+                ... # disable baloo
+            case _:
+                return None
+
         for cmd in cmd_arr:
             if uinput(self.console, f"Execute: {cmd}", 1):
                 exec_cmd(self.log, cmd, self.verbose)
