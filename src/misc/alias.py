4d7032caabc3e66a42f76be51ac0befc2a2652a2
James Aaron Erang <iaacornus.devel@gmail.com>
2023-01-24 22:00:07 +0800

    update type alias of progdata

--- src/misc/alias.py
+++ src/misc/alias.py
@@ -1,4 +1,4 @@
-ProgData = dict[str, dict[str, str]]
+ProgData = dict[str, dict[str, str, str]]
 ProgIndex = dict[int, str]
 ConfigValues = list[
         list[dict[str, str | dict[str, str]]] | dict[str, str]
