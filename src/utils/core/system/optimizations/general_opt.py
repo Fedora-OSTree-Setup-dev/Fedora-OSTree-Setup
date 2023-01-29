654ed229ed58080afbaa814aab858d128973c89e
James Aaron Erang <iaacornus.devel@gmail.com>
2023-01-29 20:57:00 +0800

    initial commit on determining whether a partition is encrypted

--- src/utils/core/system/optimizations/general_opt.py
+++ src/utils/core/system/optimizations/general_opt.py
@@ -1,4 +1,7 @@
+from typing import Any
+
 from rich.console import Console
+from psutil import disk_partitions
 
 from src.utils.shared.exec import exec_cmd
 from src.utils.shared.fetch_env import fetch_env
@@ -43,4 +46,10 @@ class SysOpt:
 
     def disable_workqueue(self) -> None:
         """Disable workqueue to improve ssd performance"""
-        ...
+
+        disk_names: list[str] = [
+                parts.device for parts in disk_partitions()
+            ]
+
+        for disks in disk_names: ...
+
