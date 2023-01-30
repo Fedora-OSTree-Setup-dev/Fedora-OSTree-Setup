644ce3b3eed0b447f0704b54ba1b5358e9d2caa6
James Aaron Erang <iaacornus.devel@gmail.com>
2023-01-30 18:57:17 +0800

    check if mypy exists or not before executing the command

--- check.sh
+++ check.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ ! $(command -v mypy &> /dev/null) ]]; then
+    echo -e "\033[1;31mmypy==0.991 is not installed!"
+    exit 1
+fi
+
 mypy --install-types
 mypy --strict $(git ls-files "*.py") --explicit-package-base
